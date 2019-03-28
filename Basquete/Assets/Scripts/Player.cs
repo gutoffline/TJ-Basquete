60748c82229d9a5bfe8bd98fcd2e7c9b3c4f4a4d
Guto Xavier <gutoffline@gmail.com>
2019-03-27 22:26:04 -0300

    Basquete

---
+++ Basquete/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bola;
+    public GameObject playerCamera;
+    public float distanciaBola = 2f;
+    //método Start é executado apenas na criação do objeto
+    void Start()
+    {
+        bola.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bola.transform.position = playerCamera.transform.position + playerCamera.transform.forward * distanciaBola;
+    }
+}
